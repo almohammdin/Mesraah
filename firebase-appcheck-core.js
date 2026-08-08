1a075ce95d7353e1dc616b238bed867b73b452f2
Naif Almohammdi <almohammdin@gmail.com>
2026-08-08 12:31:14 +0300

    Expose App Check token for native live backend

--- firebase-appcheck-core.js
+++ firebase-appcheck-core.js
@@ -1,14 +1,17 @@
 import { getApp } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import {
+  getAppCheck,
+  getToken,
   initializeAppCheck,
   ReCaptchaEnterpriseProvider
 } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-app-check.js';
 
 const RECAPTCHA_SITE_KEY = '6LdgFnstAAAAAJod6T7NgPLzkfFkSYNbc4_q4rfe';
 const app = getApp();
+let appCheck;
 
 try {
-  initializeAppCheck(app, {
+  appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaEnterpriseProvider(RECAPTCHA_SITE_KEY),
     isTokenAutoRefreshEnabled: true
   });
@@ -17,7 +20,14 @@ try {
     console.warn('Mesraah App Check core:', error);
     throw error;
   }
+  appCheck = getAppCheck(app);
 }
 
 window.MesraahFirebaseApp = app;
+window.MesraahAppCheck = appCheck;
+window.MesraahGetAppCheckToken = async ({ forceRefresh = false } = {}) => {
+  const result = await getToken(appCheck, forceRefresh);
+  if (!result?.token) throw new Error('app-check-token-missing');
+  return result.token;
+};
 window.dispatchEvent(new CustomEvent('mesraah:appcheck-ready'));
